e62b09d6c8bc867bef82efafe12bc311c195e3b9
14harish <harishpc2003@gmai.com>
2023-07-19 14:34:45 +0530

    new

--- server.py
+++ server.py
@@ -4,10 +4,11 @@ from io import BytesIO
 from base64 import b64encode
 
 app=Flask(__name__)
+val="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAAEiAQAAAAB1xeIbAAABD0lEQVR4nO3YUQ6DIAyAYRIP4JG8ukfiACYM2sLYpnMPlmXL3wdB+HhqtEBIH8QaUCgUCvUXKlhMuR9zu8Q6sqC8lbykOCWhWU2pm0C5Ks3LPUPNo4apEOaUUF9RaUWNVtJEGd6C9LoJlKdqFVmTc1a3UdepLsrX8TKG8lOWDfk9la9jnesD5a9mSU6ZkwW5Z7UZ5ax0/2knMNsSydhDhlAeqmaoec2VlWWUp6ovdSfaLUINUSX0f2QHgO1oN4S6TgWLegqw5OzWbdTFShKh5y49fG26OUINUO3Wp83JyqO7IZSX0ljic91G+arulh81RklTb9yE7uxXUR6qq8ix3i+/rduoq9RJoFAoFOrn1Q3zZ0A5hJw3NgAAAABJRU5ErkJggg=="
 
 @app.route('/')
 def Home():
-    return render_template("qr.html")
+    return render_template("qr.html",data=val)
 @app.route('/',methods=["POST"])
 def generateQR():
     memory=BytesIO()
